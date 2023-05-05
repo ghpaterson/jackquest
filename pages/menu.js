4a76ff6f6e15098b549be5bcab041d636ab73219
Graeme Paterson <ghpaterson@gmail.com>
2023-05-05 17:24:09 +0100

    no message

--- pages/menu.js
+++ pages/menu.js
@@ -7,9 +7,9 @@ export default function Menu() {
   return (
     <main>
       <NavBar />
-      <section className="font-nasa text-spaceDark text-7xl flex flex-col items-center justify-center gap-2 py-10">
+      <section className="font-nasa text-spaceDark text-5xl md:text-7xl flex flex-col items-center justify-center gap-2 py-10">
         <h1>Quest to Find...</h1>
-        <h2 className="text-4xl">High Frequency Words</h2>
+        <h2 className="text-3xl md:text-4xl">High Frequency Words</h2>
       </section>
       <section className="flex flex-col justify-center items-center gap-10 py-6 text-spaceDark text-4xl font-comic">
         <div className="bg-spaceLight p-4 rounded-xl gap-2 font-nasa">
